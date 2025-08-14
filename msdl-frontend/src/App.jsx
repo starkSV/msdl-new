50200a28337cc560c7456c87db321c7b4beed890
Shekhar Vaidya <shekhar.vaidya2@hotmail.com>
2025-08-14 13:27:16 +0530

    Project Setup

---
+++ msdl-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-slate-900 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Project Setup Complete!</h1>
+    </div>
+  )
+}
+
+export default App
